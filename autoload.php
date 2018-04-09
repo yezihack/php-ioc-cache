7b945ecd836678d584d9419860db7525083f63d0
win <win@sgfoot.com>
2018-04-09 11:15:02 +0800

    新增memcache

--- autoload.php
+++ autoload.php
@@ -6,7 +6,7 @@ header('Content-type:text/html;charset=utf-8');
  * Date: 2018/3/27
  * Time: 9:33
  */
-if (PHP_VERSION_ID <= 503000) {
+if (PHP_VERSION_ID <= 50300) {
     throw new Exception('The PHP version must be greater than or equal to 5.3.0');
 }
 spl_autoload_register(function ($className) {
